124bd57b44e5d3554538f268da7c93caaebccd0c
Филипп Кольцов <koltsovphilip@gmail.com>
2019-01-18 20:05:23 +1000

    Init commit

---
+++ DHEM_TestWork/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace DHEM_TestWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input_args = args;
+        }
+
+        static void ParseArgs(string[] args)
+        {
+        }
+    }
+}
